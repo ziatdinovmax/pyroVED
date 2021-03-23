3170cafe32bcbf54daa83db5bd9f24fccec2ff7d
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-03-23 01:02:50 +0000

    Update tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,6 +15,26 @@ sys.path.append("../../")
 from pyroved import models
 
 
+def get_traces(model, x):
+    guide_trace = pyro.poutine.trace(model.guide).get_trace(x)
+    model_trace = pyro.poutine.trace(
+        pyro.poutine.replay(model.model, trace=guide_trace)).get_trace(x)
+    return guide_trace, model_trace
+
+
+def get_enum_traces(model, x):
+    guide_enum = EnumMessenger(first_available_dim=-2)
+    model_enum = EnumMessenger()
+    guide_ = guide_enum(
+        infer.config_enumerate(model.guide, "parallel", expand=True))
+    model_ = model_enum(model.model)
+    guide_trace = poutine.trace(guide_, graph_type="flat").get_trace(x)
+    model_trace = poutine.trace(
+        pyro.poutine.replay(model_, trace=guide_trace),
+        graph_type="flat").get_trace(x)
+    return guide_trace, model_trace
+
+
 @pytest.mark.parametrize("coord", [0, 1, 2, 3])
 @pytest.mark.parametrize("data_dim", [(2, 8), (2, 8, 8), (3, 8), (3, 8, 8)])
 def test_trvae_sites_dims(data_dim, coord):
@@ -22,9 +42,7 @@ def test_trvae_sites_dims(data_dim, coord):
     if coord > 0:
         coord = coord if len(data_dim[1:]) > 1 else 1
     model = models.trVAE(data_dim[1:], coord=coord)
-    guide_trace = pyro.poutine.trace(model.guide).get_trace(x)
-    model_trace = pyro.poutine.trace(
-        pyro.poutine.replay(model.model, trace=guide_trace)).get_trace(x)
+    guide_trace, model_trace = get_traces(model, x)
     assert_equal(model_trace.nodes["latent"]['value'].shape,
                  (data_dim[0], coord+2))
     assert_equal(guide_trace.nodes["latent"]['value'].shape,
@@ -40,9 +58,7 @@ def test_trvae_sites_fn(data_dim, coord):
     if coord > 0:
         coord = coord if len(data_dim[1:]) > 1 else 1
     model = models.trVAE(data_dim[1:], coord=coord)
-    guide_trace = pyro.poutine.trace(model.guide).get_trace(x)
-    model_trace = pyro.poutine.trace(
-        pyro.poutine.replay(model.model, trace=guide_trace)).get_trace(x)
+    guide_trace, model_trace = get_traces(model, x)
     assert_(isinstance(model_trace.nodes["latent"]['fn'].base_dist, dist.Normal))
     assert_(isinstance(guide_trace.nodes["latent"]['fn'].base_dist, dist.Normal))
     assert_(isinstance(model_trace.nodes["obs"]['fn'].base_dist, dist.Bernoulli))
@@ -50,20 +66,12 @@ def test_trvae_sites_fn(data_dim, coord):
 
 @pytest.mark.parametrize("coord", [0, 1, 2, 3])
 @pytest.mark.parametrize("data_dim", [(2, 8), (2, 8, 8), (3, 8), (3, 8, 8)])
-def test_jtrvae_cont_sites_dimensions(data_dim, coord):
+def test_jtrvae_cont_sites_dims(data_dim, coord):
     x = torch.randn(*data_dim)
     if coord > 0:
         coord = coord if len(data_dim[1:]) > 1 else 1
     model = models.jtrVAE(data_dim[1:], 2, 3, coord=coord)
-    guide_enum = EnumMessenger(first_available_dim=-2)
-    model_enum = EnumMessenger()
-    guide_ = guide_enum(
-        infer.config_enumerate(model.guide, "parallel", expand=True))
-    model_ = model_enum(model.model)
-    guide_trace = poutine.trace(guide_, graph_type="flat").get_trace(x)
-    model_trace = poutine.trace(
-        pyro.poutine.replay(model_, trace=guide_trace),
-        graph_type="flat").get_trace(x)
+    guide_trace, model_trace = get_enum_traces(model, x)
     assert_equal(model_trace.nodes["latent_cont"]['value'].shape,
                  (data_dim[0], coord+2))
     assert_equal(guide_trace.nodes["latent_cont"]['value'].shape,
@@ -74,21 +82,38 @@ def test_jtrvae_cont_sites_dimensions(data_dim, coord):
 
 @pytest.mark.parametrize("coord", [0, 1, 2, 3])
 @pytest.mark.parametrize("data_dim", [(2, 8), (2, 8, 8), (3, 8), (3, 8, 8)])
-def test_jtrvae_disc_sites_dimensions(data_dim, coord):
+def test_jtrvae_disc_sites_dims(data_dim, coord):
     x = torch.randn(*data_dim)
     if coord > 0:
         coord = coord if len(data_dim[1:]) > 1 else 1
     model = models.jtrVAE(data_dim[1:], 2, 3, coord=coord)
-    guide_enum = EnumMessenger(first_available_dim=-2)
-    model_enum = EnumMessenger()
-    guide_ = guide_enum(
-        infer.config_enumerate(model.guide, "parallel", expand=True))
-    model_ = model_enum(model.model)
-    guide_trace = poutine.trace(guide_, graph_type="flat").get_trace(x)
-    model_trace = poutine.trace(
-        pyro.poutine.replay(model_, trace=guide_trace),
-        graph_type="flat").get_trace(x)
+    guide_trace, model_trace = get_enum_traces(model, x)
     assert_equal(model_trace.nodes["latent_disc"]['value'].shape,
                  (3, data_dim[0], 3))
     assert_equal(guide_trace.nodes["latent_disc"]['value'].shape,
                  (3, data_dim[0], 3))
+
+
+@pytest.mark.parametrize("coord", [0, 1, 2, 3])
+@pytest.mark.parametrize("data_dim", [(2, 8), (2, 8, 8), (3, 8), (3, 8, 8)])
+def test_jtrvae_cont_sites_fn(data_dim, coord):
+    x = torch.randn(*data_dim)
+    if coord > 0:
+        coord = coord if len(data_dim[1:]) > 1 else 1
+    model = models.jtrVAE(data_dim[1:], 2, 3, coord=coord)
+    guide_trace, model_trace = get_enum_traces(model, x)
+    assert_(isinstance(model_trace.nodes["latent_cont"]['fn'].base_dist, dist.Normal))
+    assert_(isinstance(guide_trace.nodes["latent_cont"]['fn'].base_dist, dist.Normal))
+    assert_(isinstance(model_trace.nodes["obs"]['fn'].base_dist, dist.Bernoulli))
+
+
+@pytest.mark.parametrize("coord", [0, 1, 2, 3])
+@pytest.mark.parametrize("data_dim", [(2, 8), (2, 8, 8), (3, 8), (3, 8, 8)])
+def test_jtrvae_disc_sites_fn(data_dim, coord):
+    x = torch.randn(*data_dim)
+    if coord > 0:
+        coord = coord if len(data_dim[1:]) > 1 else 1
+    model = models.jtrVAE(data_dim[1:], 2, 3, coord=coord)
+    guide_trace, model_trace = get_enum_traces(model, x)
+    assert_(isinstance(model_trace.nodes["latent_disc"]['fn'], dist.OneHotCategorical))
+    assert_(isinstance(guide_trace.nodes["latent_disc"]['fn'], dist.OneHotCategorical))
