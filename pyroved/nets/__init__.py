0ecbed8522dd067f748f0fe88444c1886e3381aa
ziatdinovmax <ziatdinovmax@gmail.com>
2021-03-27 00:16:48 -0400

    Update import statements

--- pyroved/nets/__init__.py
+++ pyroved/nets/__init__.py
@@ -1,5 +1,7 @@
 from .fc import (fcClassifierNet, fcDecoderNet, fcEncoderNet, jfcEncoderNet,
                  sDecoderNet)
+from .conv import ConvBlock, UpsampleBlock, FeatureExtractor, Upsampler
 
 __all__ = ["fcEncoderNet", "fcDecoderNet", "sDecoderNet",
-           "fcClassifierNet", "jfcEncoderNet"]
+           "fcClassifierNet", "jfcEncoderNet", "ConvBlock", "UpsampleBlock",
+           "FeatureExtractor", "Upsampler"]
