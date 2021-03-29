3aafe4e44113e1d1af9bc7c00a214b03eb797b6f
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-03-29 02:28:33 +0000

    Update imports

--- pyroved/models/__init__.py
+++ pyroved/models/__init__.py
@@ -3,6 +3,7 @@ Variational autoencoder and encoder-decoder models
 """
 from .trvae import trVAE
 from .sstrvae import sstrVAE
-from.jtrvae import jtrVAE
+from .jtrvae import jtrVAE
+from .ved import VED
 
-__all__ = ['trVAE', 'jtrVAE', 'sstrVAE']
+__all__ = ['trVAE', 'jtrVAE', 'sstrVAE', 'VED']
