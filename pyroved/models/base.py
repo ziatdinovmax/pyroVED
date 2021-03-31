597a14ffc4b0275d17d845eb929f818b0259fb1e
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-03-31 06:24:20 +0000

    Linting

--- pyroved/models/base.py
+++ pyroved/models/base.py
@@ -29,12 +29,12 @@ class baseVAE(nn.Module):
         self.grid = None
 
     @abstractmethod
-    def model(self, x, y, **kwargs):
+    def model(self, x, y=None, **kwargs):
         """Pyro's model"""
         raise NotImplementedError
 
     @abstractmethod
-    def guide(self, x, y, **kwargs):
+    def guide(self, x, y=None, **kwargs):
         """Pyro's guide"""
         raise NotImplementedError
 
