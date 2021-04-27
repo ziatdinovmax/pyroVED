0fba9a1962f873f9fab4bb1698db16ac22f510a0
Matthew Carbone <x94carbone@gmail.com>
2021-04-27 11:18:23 -0400

    Document get_sampler in utils.prob.py

--- pyroved/utils/prob.py
+++ pyroved/utils/prob.py
@@ -2,16 +2,41 @@ from typing import Type
 import pyro.distributions as dist
 
 
-def get_sampler(sampler: str, **kwargs) -> Type[dist.Distribution]:
+def get_sampler(
+    sampler: str, normal_dist_scale: float = 0.5
+) -> Type[dist.Distribution]:
+    """Gets a sampler for VAE's decoder.
+
+    [description]
+
+    Parameters
+    ----------
+    sampler : {'bernoulli', 'continuous_bernoulli', 'gaussian'}
+        [description]
+    normal_dist_scale : {float}, optional
+        Only used in the case of a Gaussian distribution. This is the scale
+        parameter for the Gaussian distribution. (The default is 0.5).
+
+    Returns
+    -------
+    Type[pyro.distributions.Distribution]
+
+    Raises
+    ------
+    KeyError
+        If the provided sampler key is not a valid distribution.
     """
-    Get a sampler for VAE's decoder
-    """
+
     samplers = {
         "bernoulli": lambda x: dist.Bernoulli(x, validate_args=False),
         "continuous_bernoulli": lambda x: dist.ContinuousBernoulli(x),
-        "gaussian": lambda x: dist.Normal(x, kwargs.get("decoder_sig", 0.5))
-        }
+        "gaussian": lambda x: dist.Normal(x, normal_dist_scale)
+    }
+
     if sampler not in samplers.keys():
-        raise NotImplementedError(
-            "Select between the following decoder samplers: {}".format(list(samplers.keys())))
+        raise KeyError(
+            "Select between the following decoder "
+            "samplers: {}".format(list(samplers.keys()))
+        )
+
     return samplers[sampler]
