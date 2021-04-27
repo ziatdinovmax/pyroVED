55e226a7533923dd3deec422741883bdc81f39c8
Matthew Carbone <x94carbone@gmail.com>
2021-04-27 11:22:39 -0400

    Rollback change to optional argument name in

--- pyroved/utils/prob.py
+++ pyroved/utils/prob.py
@@ -3,7 +3,7 @@ import pyro.distributions as dist
 
 
 def get_sampler(
-    sampler: str, normal_dist_scale: float = 0.5
+    sampler: str, decoder_sig: float = 0.5
 ) -> Type[dist.Distribution]:
     """Gets a sampler for VAE's decoder.
 
@@ -13,7 +13,7 @@ def get_sampler(
     ----------
     sampler : {'bernoulli', 'continuous_bernoulli', 'gaussian'}
         [description]
-    normal_dist_scale : {float}, optional
+    decoder_sig : {float}, optional
         Only used in the case of a Gaussian distribution. This is the scale
         parameter for the Gaussian distribution. (The default is 0.5).
 
@@ -30,7 +30,7 @@ def get_sampler(
     samplers = {
         "bernoulli": lambda x: dist.Bernoulli(x, validate_args=False),
         "continuous_bernoulli": lambda x: dist.ContinuousBernoulli(x),
-        "gaussian": lambda x: dist.Normal(x, normal_dist_scale)
+        "gaussian": lambda x: dist.Normal(x, decoder_sig)
     }
 
     if sampler not in samplers.keys():
